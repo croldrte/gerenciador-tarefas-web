6c3493c3f06a05b5157516c476b2179223a3f901
Carol Duarte <carolduarte2000@gmail.com>
2025-06-23 14:07:17 -0300

    [fix] Corrige nome do DbContext no Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<SeuDbContext>();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
 
