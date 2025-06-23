c8702433bbc4c482e86b8847a220d5fa05f3357f
Carol Duarte <carolduarte2000@gmail.com>
2025-06-23 14:04:25 -0300

    [feat] Aplica migrations automaticamente ao iniciar a aplicação

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SeuDbContext>();
+    db.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
