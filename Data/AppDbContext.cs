5ebbbdc8516c37edf9571e1c2847b9827492b5c8
Carol Duarte <carolduarte2000@gmail.com>
2025-07-09 08:16:29 -0300

    [chore] Atualiza migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -28,60 +28,60 @@ namespace TaskManager.Data
                     Id = 1,
                     Title = "Enviar relatório",
                     Description = "Mandar pra Amanda: revisão detalhada dos contratos com fornecedores, ciclos de recebimento, dívidas de curto prazo.",
-                    Date = DateTime.Today.AddDays(2),
+                    Date = new DateTime(2025, 7, 11),
                     Time = new TimeSpan(9, 0, 0),
                     IsCompleted = true,
                     IsImportant = true,
                     CategoryId = 1,
-                    CreatedAt = DateTime.Today.AddDays(-5)
+                    CreatedAt = new DateTime(2025, 7, 9, 8, 0, 0)
                 },
                 new Models.Task
                 {
                     Id = 2,
                     Title = "Prova de economia",
                     Description = "Oferta e demanda, estruturas de mercado, custo de produção, teorias econômicas. Capítulos 6 a 9.",
-                    Date = DateTime.Today.AddDays(1),
+                    Date = new DateTime(2025, 7, 10),
                     Time = new TimeSpan(18, 0, 0),
                     IsCompleted = false,
                     IsImportant = false,
                     CategoryId = 2,
-                    CreatedAt = DateTime.Today.AddDays(-4)
+                    CreatedAt = new DateTime(2025, 7, 9, 8, 0, 0)
                 },
                 new Models.Task
                 {
                     Id = 3,
                     Title = "Fazer yoga",
                     Description = "Pranayama – 5 minutos\r\nCão olhando para baixo – 3 minutos\r\nGato e vaca – 2 minutos\r\nGuerreiro 1 – 2 minutos\r\nGuerreiro 2 – 2 minutos\r\nPostura da criança – 3 minutos\r\nSavasana – 5 minutos",
-                    Date = DateTime.Today,
+                    Date = new DateTime(2025, 7, 9),
                     Time = new TimeSpan(7, 30, 0),
                     IsCompleted = false,
                     IsImportant = false,
                     CategoryId = 3,
-                    CreatedAt = DateTime.Today.AddDays(-3)
+                    CreatedAt = new DateTime(2025, 7, 9, 8, 0, 0)
                 },
                 new Models.Task
                 {
                     Id = 4,
                     Title = "Ir ao mercado",
                     Description = "Arroz\r\nFeijão\r\nMacarrão\r\nAveia\r\nPão integral\r\nLeite\r\nOvos\r\nBanana\r\nMaçã\r\nCenoura\r\nBrócolis\r\nTomate\r\nAlho\r\nCebola\r\nSabonete\r\nShampoo\r\nPasta de dente\r\nPapel higiênico\r\nCafé",
-                    Date = DateTime.Today.AddDays(-1),
+                    Date = new DateTime(2025, 7, 8),
                     Time = new TimeSpan(20, 0, 0),
                     IsCompleted = false,
                     IsImportant = false,
                     CategoryId = 4,
-                    CreatedAt = DateTime.Today.AddDays(-2)
+                    CreatedAt = new DateTime(2025, 7, 9, 8, 0, 0)
                 },
                 new Models.Task
                 {
                     Id = 5,
                     Title = "Enviar o TCC para revisão",
                     Description = "Keynes, oferta e demanda, política fiscal. Formatar referências. E-mail: juvenal@universidade.com.",
-                    Date = DateTime.Today.AddDays(7),
+                    Date = new DateTime(2025, 7, 16),
                     Time = new TimeSpan(23, 0, 0),
                     IsCompleted = false,
                     IsImportant = true,
                     CategoryId = 2,
-                    CreatedAt = DateTime.Today.AddDays(-1)
+                    CreatedAt = new DateTime(2025, 7, 9, 8, 0, 0)
                 }
             );
         }
